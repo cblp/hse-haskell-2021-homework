550eea05f941c3baef57505dba9f36f669894833
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-02-07 22:38:11 +0300

    Add Task8 stub and about tests

---
+++ Task8_Fix.hs
@@ -0,0 +1,21 @@
+module Task8_Fix where
+
+-- import           Test.Tasty.QuickCheck
+
+iterateElement :: a -> [a]
+iterateElement = undefined
+
+-- prop_iterateElement :: Integer -> Property
+-- prop_iterateElement x = iterateElement x === [x, x..]
+
+fibonacci :: Integer -> Integer
+fibonacci = undefined
+
+factorial :: Integer -> Integer
+factorial = undefined
+
+mapFix :: (a -> b) -> [a] -> [b]
+mapFix = undefined
+
+-- prop_mapFix_eq_map :: Blind (Integer -> Char) -> [Integer] -> Property
+-- prop_mapFix_eq_map (Blind f) xs = mapFix f xs === map f xs
