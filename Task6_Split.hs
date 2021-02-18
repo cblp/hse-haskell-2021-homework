890a6b9439e2d3673566d77175c8ea68d050523b
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-02-18 13:24:21 +0300

    Task6: Add stubs

--- Task6_Split.hs
+++ Task6_Split.hs
@@ -1,4 +1,19 @@
 module Task6_Split where
 
+-- import           Test.QuickCheck
+-- import           Test.Tasty.HUnit
+
 -- splitOn :: a -> [a] -> _
 -- splitOn = undefined
+
+-- unit_splitOn :: Assertion
+-- unit_splitOn = splitOn '/' "path/to/file" @?= ["path", "to", "file"]
+
+joinWith :: a -> [[a]] -> [a]
+joinWith = undefined
+
+-- unit_joinWith :: Assertion
+-- unit_joinWith = joinWith '/' ["path", "to", "file"] @?= "path/to/file"
+
+-- prop_splitOn_joinWith :: Char -> String -> Property
+-- prop_splitOn_joinWith c s = joinWith c (splitOn c s) === s
